f5601c54a494b753fcaa38cf7082dc384b2ba712
Jasly D <JaslyD88@gmail.com>
2025-03-29 13:47:26 -0400

    Add files via upload

---
+++ A Select Profile/profile.js
@@ -0,0 +1,8 @@
+const profiler = () => {
+    $("#name").text("Jasly");
+    $("#major").text("CGT");
+    $("#year").text("Sophomore");
+    $("h4").html("<em>USA</em>");
+}
+
+$(document).ready(profiler);
